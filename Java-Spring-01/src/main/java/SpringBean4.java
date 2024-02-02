cce02c58e76c84de69eb77924e4ac925588e7673
punchihewa <navishkapunchihewa66@gmail.com>
2024-02-02 15:53:09 +0530

    update xml file spring

--- Java-Spring-01/src/main/java/SpringBean4.java
+++
@@ -1,5 +0,0 @@
-public class SpringBean4 {
-    public SpringBean4() {
-        System.out.println("Spring 4  instance is invoke");
-    }
-}
---
+++ Java-Spring-01/src/main/java/lk/ijse/gdse/api/bean/SpringBean4.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.api.bean;
+
+public class SpringBean4 {
+    public SpringBean4() {
+        System.out.println("SpringThere instance is invoke");
+    }
+}

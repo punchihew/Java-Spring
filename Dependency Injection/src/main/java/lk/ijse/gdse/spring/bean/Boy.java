ead853c4a3e90e535a9a287f2e2ee7b97923034d
punchihewa <navishkapunchihewa66@gmail.com>
2024-02-09 16:43:52 +0530

    depenjency injectionss

--- Dependency Injection/src/main/java/lk/ijse/gdse/spring/bean/Boy.java
+++ Dependency Injection/src/main/java/lk/ijse/gdse/spring/bean/Boy.java
@@ -1,12 +1,16 @@
 package lk.ijse.gdse.spring.bean;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 @Component("lahiru")
-public class Boy {
+public class Boy  implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
 
     @Autowired
     private GoodGirl goodGirl;
@@ -19,9 +23,34 @@ public class Boy {
         sachini.love();
     }
 
-    @PostConstruct
+    @PostConstruct//popolete proprties eka gvawadi thamai add wenne controcter ekfi dd wenne nh depencies injectionn
     public void initialize(){
         System.out.println("Have I Got aGirl Friend..." + goodGirl);
+
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+    }
+
+    @Override
+    public void setBeanName(String s) {
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 
     }
 }

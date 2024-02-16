0c858ee6909e7ffcf50595520d746ce27cedec55
punchihewa <navishkapunchihewa66@gmail.com>
2024-02-16 15:28:27 +0530

    update

--- Dependency Injection/src/main/java/lk/ijse/gdse/spring/AppConfig.java
+++ Dependency Injection/src/main/java/lk/ijse/gdse/spring/AppConfig.java
@@ -1,5 +1,6 @@
 package lk.ijse.gdse.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,9 +9,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-    public AppConfig() {
-        System.out.println("appConfig instance is created");
-    }
-
+//    public AppConfig() {
+//        System.out.println("appConfig instance is created");
+//    }
+    @Bean //bean definition using object
+      public String myString1(){
+          return "IJSE";
+      }
 
     }
--- Dependency Injection/src/main/java/lk/ijse/gdse/spring/bean/SpringBean.java
+++ Dependency Injection/src/main/java/lk/ijse/gdse/spring/bean/SpringBean.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class SpringBean {
 
-    public SpringBean() {
+    public SpringBean(String name) {
         System.out.println("SpringBean() -- Constructor");
     }
 }

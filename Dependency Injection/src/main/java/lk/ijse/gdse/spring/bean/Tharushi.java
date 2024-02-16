3d00b5f031772762aec6dc298233673d7a4e49b0
punchihewa <navishkapunchihewa66@gmail.com>
2024-02-16 12:17:45 +0530

    add primary

--- Dependency Injection/src/main/java/lk/ijse/gdse/spring/bean/Tharushi.java
+++ Dependency Injection/src/main/java/lk/ijse/gdse/spring/bean/Tharushi.java
@@ -1,8 +1,10 @@
 package lk.ijse.gdse.spring.bean;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class Tharushi implements GoodGirl {
 
     @Override

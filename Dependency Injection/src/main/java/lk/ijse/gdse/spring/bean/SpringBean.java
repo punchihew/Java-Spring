d0fa3a9586879b35a0bf33c7d9c169fd8c46c95a
punchihewa <navishkapunchihewa66@gmail.com>
2024-02-16 15:54:07 +0530

    update

--- Dependency Injection/src/main/java/lk/ijse/gdse/spring/bean/SpringBean.java
+++ Dependency Injection/src/main/java/lk/ijse/gdse/spring/bean/SpringBean.java
@@ -1,12 +1,35 @@
 package lk.ijse.gdse.spring.bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Array;
+import java.util.*;
 
 @Component
 public class SpringBean {
 
-    public SpringBean(String name) {
-        System.out.println("SpringBean() -- Constructor---" + name);
+//    public SpringBean(String[] nameList) {
+//        System.out.println("SpringBean() -- Constructor---" + nameList);
+//        System.out.println(Arrays.toString(nameList));
+//    }
+
+//    @Autowired
+//    public SpringBean(Map<String,String> nameList) {
+//        System.out.println("SpringBean() -- Constructor---" + nameList);
+//        System.out.println(nameList);
+//    }
+
+//    @Autowired
+//    public SpringBean(List<String> nameList) {
+//        System.out.println("SpringBean() -- Constructor---" + nameList);
+//        System.out.println(nameList);
+//    }
+
+    @Autowired
+    public SpringBean(Set<String> nameList) {
+        System.out.println("SpringBean() -- Constructor---" + nameList);
+        System.out.println( nameList);
     }
 
 }

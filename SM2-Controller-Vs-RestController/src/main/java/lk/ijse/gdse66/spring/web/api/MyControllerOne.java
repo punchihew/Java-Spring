8b44c6c6b5fba83fc42aa0985cb5aa9b3a63d369
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-01 15:33:59 +0530

    update

---
+++ SM2-Controller-Vs-RestController/src/main/java/lk/ijse/gdse66/spring/web/api/MyControllerOne.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse66.spring.web.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyControllerOne {
+    public MyControllerOne() {
+        System.out.println("MyControllerOne()-- Contractor");
+    }
+}

c7092e58f39d7e95b7789d83f40155b1dfab4384
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-01 10:43:23 +0530

    spring Mvc

--- SM1-Spring-Web-Mvc/src/main/java/lk/ijse/gdse66/HelloSpringWebMvc/WebAppConfig.java
+++ SM1-Spring-Web-Mvc/src/main/java/lk/ijse/gdse66/HelloSpringWebMvc/WebAppConfig.java
@@ -8,4 +8,5 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Component
 @EnableWebMvc
 public class WebAppConfig {
+
 }
---
+++ SM1-Spring-Web-Mvc/src/main/java/lk/ijse/gdse66/HelloSpringWebMvc/bean/SpringBean.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse66.HelloSpringWebMvc.bean;
+
+public class SpringBean {
+    public SpringBean() {
+        System.out.println("SpringBean()");
+    }
+}

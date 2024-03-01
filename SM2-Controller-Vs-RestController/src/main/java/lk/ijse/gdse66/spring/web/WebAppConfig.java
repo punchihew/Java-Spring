89abca5b63cdc1ea9ac994390ccf9b4014218c68
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-01 15:50:39 +0530

    update

--- SM2-Controller-Vs-RestController/src/main/java/lk/ijse/gdse66/spring/web/WebAppConfig.java
+++ SM2-Controller-Vs-RestController/src/main/java/lk/ijse/gdse66/spring/web/WebAppConfig.java
@@ -1,14 +1,20 @@
 package lk.ijse.gdse66.spring.web;
 
+import lk.ijse.gdse66.spring.web.api.MyControllerOne;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan
 @EnableWebMvc
 public class WebAppConfig {
     public WebAppConfig() {
         System.out.println("WebAppConfig () -- Contractor");
+    }
+
+    @Bean
+    public MyControllerOne myControllerOne(){
+        return new MyControllerOne();
     }
 }
--- SM2-Controller-Vs-RestController/src/main/java/lk/ijse/gdse66/spring/web/api/MyControllerTwo.java
+++ SM2-Controller-Vs-RestController/src/main/java/lk/ijse/gdse66/spring/web/api/MyControllerTwo.java
@@ -1,2 +1,10 @@
-package lk.ijse.gdse66.spring.web.api;public class MyControllerTwo {
+package lk.ijse.gdse66.spring.web.api;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyControllerTwo {
+    public MyControllerTwo() {
+        System.out.println("MyControllerTwo()----Constructor");
+    }
 }

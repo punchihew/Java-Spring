f396e78f207fe9adb43a423e5627eb508128f44a
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-01 15:28:47 +0530

    Controller Vs rest Controoler

---
+++ SM2-Controller-Vs-RestController/src/main/java/lk/ijse/gdse66/spring/web/WebAppConfig.java
@@ -0,0 +1,12 @@
+package lk.ijse.gdse66.spring.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan
+@EnableWebMvc
+public class WebAppConfig {
+
+}
---
+++ SM2-Controller-Vs-RestController/src/main/java/lk/ijse/gdse66/spring/web/WebAppInitializer.java
@@ -0,0 +1,22 @@
+package lk.ijse.gdse66.spring.web;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override//context eka hadagnna awasha details
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override//context eka hadagnna awasha details
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override//
+    protected String[] getServletMappings() {
+        return new String[]{"/"}; //default mapping
+    }
+}

57b1f6c2e8cbd3234b6ed4042c6840d95058cf5c
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-01 16:25:00 +0530

    update

--- SM2-Controller-Vs-RestController/src/main/java/lk/ijse/gdse66/spring/web/WebAppConfig.java
+++ SM2-Controller-Vs-RestController/src/main/java/lk/ijse/gdse66/spring/web/WebAppConfig.java
@@ -5,13 +5,19 @@ import lk.ijse.gdse66.spring.web.api.MyControllerTwo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 public class WebAppConfig {
     public WebAppConfig() {
         System.out.println("WebAppConfig () -- Contractor");
+    }
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        return resolver;
     }
 
     @Bean

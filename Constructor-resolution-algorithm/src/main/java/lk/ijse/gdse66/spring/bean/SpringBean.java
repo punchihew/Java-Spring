6c30ba6456879b937a8ff4b0f57dcdb61ecb35ef
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-01 09:18:54 +0530

    contrutoer algorithm

--- Constructor-resolution-algorithm/src/main/java/lk/ijse/gdse66/spring/bean/SpringBean.java
+++ Constructor-resolution-algorithm/src/main/java/lk/ijse/gdse66/spring/bean/SpringBean.java
@@ -1,5 +1,7 @@
 package lk.ijse.gdse66.spring.bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,17 +10,39 @@ public class SpringBean {
     private String name;
     private int age;
     private boolean isMale;
+    private float temp;
 
+    //no arges contractor
 //    public SpringBean() {
 //        System.out.println("Spring Bean Constructor");
 //    }
 
-    //all ares contractor
+//    all ares contractor
+    @Autowired(required = false)
     public SpringBean(String name, int age, boolean isMale) {
         this.name = name;
         this.age = age;
         this.isMale = isMale;
         System.out.println("SpringBean(String,int,boolean");
         System.out.printf("name=%s,age=%s,isMale=%s\n",name,age,isMale);
+    }
+
+    //full args contractor
+    @Autowired(required = false)
+    public SpringBean(String name, int age, boolean isMale,@Nullable float temp) {
+        this.name = name;
+        this.age = age;
+        this.isMale = isMale;
+        this.temp = temp;
+        System.out.println("SpringBean(String,int,boolean,float");
+        System.out.printf("name=%s,age=%s,isMale=%s,temp=%s\n",name,age,isMale,temp);
+    }
+
+    @Autowired(required = false)
+    public SpringBean(String name, int age) {
+        this.name = name;
+        this.age = age;
+        System.out.println("SpringBean(String,int");
+        System.out.printf("name=%s,age=%s\n",name,age);
     }
 }

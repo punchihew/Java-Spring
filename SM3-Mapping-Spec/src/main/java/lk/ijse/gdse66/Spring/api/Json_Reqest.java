98ff18d58bcbab18e250d0f341db9a8767f89cb0
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-07 15:39:03 +0530

    update

--- SM3-Mapping-Spec/src/main/java/lk/ijse/gdse66/Spring/api/Json_Reqest.java
+++ SM3-Mapping-Spec/src/main/java/lk/ijse/gdse66/Spring/api/Json_Reqest.java
@@ -2,17 +2,25 @@ package lk.ijse.gdse66.Spring.api;
 
 
 import lk.ijse.gdse66.Spring.dto.CustomerDto;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/json")
 public class Json_Reqest {
 
-    @PostMapping
-    public String SaveCustomer(String id , String name , int age){
-        return String.format("SaveCustomer(String id , String name , int age) : %s.%s.%s",id,name,age);
+//    @PostMapping
+//    public String SaveCustomer(String id , String name , int age){
+//        return String.format("SaveCustomer(String id , String name , int age) : %s.%s.%s",id,name,age);
+//    }
+
+//    @PostMapping
+//    public String SaveCustomer(@RequestBody CustomerDto customer){
+//        return customer.toString();
+//    }
+
+
+    @GetMapping(produces = "application/json")
+    public CustomerDto getSuplier(){
+        return new CustomerDto("C001","Navishka",20);
     }
 }

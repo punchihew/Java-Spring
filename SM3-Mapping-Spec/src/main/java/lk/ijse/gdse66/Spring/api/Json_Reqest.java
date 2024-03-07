78bb8a6fc2da6624ffd3bbf79b8c3f71d8ae1ef0
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-07 15:46:16 +0530

    update

--- SM3-Mapping-Spec/src/main/java/lk/ijse/gdse66/Spring/api/Json_Reqest.java
+++ SM3-Mapping-Spec/src/main/java/lk/ijse/gdse66/Spring/api/Json_Reqest.java
@@ -2,7 +2,10 @@ package lk.ijse.gdse66.Spring.api;
 
 
 import lk.ijse.gdse66.Spring.dto.CustomerDto;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/json")
@@ -22,5 +25,16 @@ public class Json_Reqest {
     @GetMapping(produces = "application/json")
     public CustomerDto getSuplier(){
         return new CustomerDto("C001","Navishka",20);
+    }
+
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ArrayList<CustomerDto> GetAllSuplier(){
+        ArrayList<CustomerDto> customerDtos = new ArrayList<>();
+        customerDtos.add(new CustomerDto("C001","Navishka",20));
+        customerDtos.add(new CustomerDto("C002","Punchihewa",21));
+        customerDtos.add(new CustomerDto("C003","Kamal",22));
+
+        return customerDtos;
     }
 }

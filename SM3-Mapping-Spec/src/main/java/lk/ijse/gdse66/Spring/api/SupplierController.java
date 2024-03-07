62790b1ae18cafc7c17231f08d232d22a20787b5
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-07 14:25:16 +0530

    update

--- SM3-Mapping-Spec/src/main/java/lk/ijse/gdse66/Spring/api/SupplierController.java
+++ SM3-Mapping-Spec/src/main/java/lk/ijse/gdse66/Spring/api/SupplierController.java
@@ -1,10 +1,17 @@
 package lk.ijse.gdse66.Spring.api;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/supplier")
 public class SupplierController {
+
+    @PostMapping
+    public String SaveCustomer(String id , String name , int age){
+        return String.format("SaveCustomer(String id,String Name,int Id) :%s,%s,%s" , id,name,age) ;
+    }
 }

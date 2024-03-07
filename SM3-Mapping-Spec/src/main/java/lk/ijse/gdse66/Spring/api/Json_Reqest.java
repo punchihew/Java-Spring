e58e59215ee0ae0332e6ba37eb19ca0a4bee0079
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-07 15:02:34 +0530

    update

---
+++ SM3-Mapping-Spec/src/main/java/lk/ijse/gdse66/Spring/api/Json_Reqest.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse66.Spring.api;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/json")
+public class Json_Reqest {
+}
--- SM3-Mapping-Spec/src/main/java/lk/ijse/gdse66/Spring/api/SupplierController.java
+++ SM3-Mapping-Spec/src/main/java/lk/ijse/gdse66/Spring/api/SupplierController.java
@@ -1,17 +1,20 @@
 package lk.ijse.gdse66.Spring.api;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.gdse66.Spring.dto.CustomerDto;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/supplier")
 public class SupplierController {
 
+//    @PostMapping
+//    public String SaveCustomer(String id , String name , int age){
+//        return String.format("SaveCustomer(String id,String Name,int Id) :%s,%s,%s" , id,name,age) ;
+//    }
+
     @PostMapping
-    public String SaveCustomer(String id , String name , int age){
-        return String.format("SaveCustomer(String id,String Name,int Id) :%s,%s,%s" , id,name,age) ;
+    public String SaveCustomer(@ModelAttribute CustomerDto customer){
+        return customer.toString();
     }
 }

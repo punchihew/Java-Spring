b73e6c2a5bd76ab2b2541053fb3fa5459f02c090
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-07 15:02:59 +0530

    update

--- SM3-Mapping-Spec/src/main/java/lk/ijse/gdse66/Spring/api/Json_Reqest.java
+++ SM3-Mapping-Spec/src/main/java/lk/ijse/gdse66/Spring/api/Json_Reqest.java
@@ -1,10 +1,18 @@
 package lk.ijse.gdse66.Spring.api;
 
 
+import lk.ijse.gdse66.Spring.dto.CustomerDto;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/json")
 public class Json_Reqest {
+
+    @PostMapping
+    public String SaveCustomer(@ModelAttribute CustomerDto customer){
+        return customer.toString();
+    }
 }

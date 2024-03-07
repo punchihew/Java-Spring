e7fcdc0744b94ab81e0e91dbd377ac6233ce4bab
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-07 10:09:16 +0530

    mapping spec

---
+++ SM3-Mapping-Spec/src/main/java/lk/ijse/gdse66/Spring/WebAppConfig.java
@@ -0,0 +1,19 @@
+package lk.ijse.gdse66.Spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class WebAppConfig implements WebMvcConfigurer {
+
+    public WebAppConfig() {
+        System.out.println("WebAppConfig () -- Contractor");
+    }
+
+}

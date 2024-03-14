3b07ae686c1c235c09015590ae19555f4e6b7a0d
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-14 15:47:00 +0530

    final Spring

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/api/CustomerController.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/api/CustomerController.java
@@ -1,4 +1,13 @@
 package lk.ijse.gdse66.spring.api;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
+
+    public String myMethoad(){
+        return "Hi";
+    }
 }

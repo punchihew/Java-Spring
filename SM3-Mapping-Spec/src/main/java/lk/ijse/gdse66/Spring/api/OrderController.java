8be5b0eb52a727278e4b95ce7f2327eb55f7d87f
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-14 09:25:18 +0530

    update new

--- SM3-Mapping-Spec/src/main/java/lk/ijse/gdse66/Spring/api/OrderController.java
+++ SM3-Mapping-Spec/src/main/java/lk/ijse/gdse66/Spring/api/OrderController.java
@@ -2,6 +2,7 @@ package lk.ijse.gdse66.Spring.api;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     @GetMapping
-    public String Getorders(String token){
+    public String Getorders(@RequestHeader String token){
         return String.format("Getorders(String Token) :%s",token);
     }
 }

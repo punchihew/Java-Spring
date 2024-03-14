90c67afe244922306847c1017a6cde8154695ed5
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-14 15:44:38 +0530

    final Spring

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/WebAppConfig.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/WebAppConfig.java
@@ -6,6 +6,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
+@Configuration
+@EnableWebMvc
+@ComponentScan
 public class WebAppConfig implements WebMvcConfigurer {
     public WebAppConfig() {
         System.out.println("WebAppConfig () -- Contractor");

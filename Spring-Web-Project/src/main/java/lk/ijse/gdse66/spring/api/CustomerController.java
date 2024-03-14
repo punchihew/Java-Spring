06a14942017970e64083111c485f8e305bf4f1c1
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-14 16:15:22 +0530

    final Spring

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/api/CustomerController.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/api/CustomerController.java
@@ -2,12 +2,14 @@ package lk.ijse.gdse66.spring.api;
 
 import lk.ijse.gdse66.spring.dto.CustomerDto;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {

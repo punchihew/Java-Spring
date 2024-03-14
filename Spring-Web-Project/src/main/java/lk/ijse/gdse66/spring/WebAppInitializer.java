d58514ad6b6eeb7313adb4039397c6d9e1de4ce3
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-14 15:42:57 +0530

    final Spring

---
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/WebAppInitializer.java
@@ -0,0 +1,26 @@
+package lk.ijse.gdse66.spring;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    public WebAppInitializer() {
+        System.out.println("WebAppInitializer () -- Contractor");
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/WebRootConfig.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse66.spring;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebRootConfig {
+    public WebRootConfig() {
+        System.out.println("WebRootConfig () -- Contractor");
+    }
+}

55b6adfb6392bc639d1a3e46d90b6413267014e8
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-25 09:58:10 +0530

    update 17 java

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/services/CustomerService.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/services/CustomerService.java
@@ -1,4 +1,19 @@
 package lk.ijse.gdse66.spring.services;
 
+import lk.ijse.gdse66.spring.dto.CustomerDto;
+
+import java.util.List;
+
 public interface CustomerService {
+
+    List<CustomerDto> getAllCustomer();
+
+    CustomerDto getCustomerDetails(String id);
+
+    void saveCustomer(CustomerDto customerDto);
+
+    void  updateCustomer(CustomerDto customerDto);
+
+    void deleteCustomer(String id);
+
 }

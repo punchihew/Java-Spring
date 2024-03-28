25d800ec8da0df6e662fb9acb4fcb9afb40c834e
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-28 10:53:27 +0530

    update

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/api/CustomerController.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/api/CustomerController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api/v1/customer")
+@RequestMapping("/api/v1/customers")
 public class CustomerController {
 
     @Autowired

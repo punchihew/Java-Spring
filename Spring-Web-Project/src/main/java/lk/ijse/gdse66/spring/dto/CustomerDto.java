bb5fc789168d7f43bbea9d6c7b9354ee781c7c7d
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-28 11:52:41 +0530

    update

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/dto/CustomerDto.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/dto/CustomerDto.java
@@ -1,5 +1,7 @@
 package lk.ijse.gdse66.spring.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Null;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,12 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerDto implements Serializable {
+
+    @Null(message = "Id Is Auto Generated")
     private String id;
+    @NotBlank(message = "Name Cannot Been Null")
     private String name;
+    @NotBlank(message = "Address Cannot Been Null")
     private String address;
 
 

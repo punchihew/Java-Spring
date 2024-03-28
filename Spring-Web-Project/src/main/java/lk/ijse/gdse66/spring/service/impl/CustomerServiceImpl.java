e783922eb0ff8f68b86bf5cb63a0fe66b45759bc
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-28 10:39:55 +0530

    update

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/impl/CustomerServiceImpl.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/service/impl/CustomerServiceImpl.java
@@ -34,13 +34,17 @@ public class CustomerServiceImpl implements lk.ijse.gdse66.spring.service.Custom
 //        Stream<Customer> stream = customerList.stream();
 //        Stream<CustomerDto> objectStream = stream.map(customer -> transformer.fromCustomerEntity(customer));
 //        List<CustomerDto> collect = objectStream.collect(Collectors.toList());
-
         return repo.findAll().stream() //strem eke coustomerla polimata hadanwa
                 .map(customer -> transformer.fromCustomerEntity(customer)).toList();
+
     }
 
     @Override
     public CustomerDto getCustomerDetails(String id) {
+
+        if (!repo.existsById(id)){
+            throw new RuntimeException("Customer Id :" + id +"Dones not exis");
+        }
         return transformer.fromCustomerEntity(repo.findById(id).get());
     }
 
@@ -53,11 +57,17 @@ public class CustomerServiceImpl implements lk.ijse.gdse66.spring.service.Custom
 
     @Override
     public void updateCustomer(CustomerDto customerDTO) {
+        if (!repo.existsById(customerDTO.getId())){
+            throw new RuntimeException("Update Filled Id :" + customerDTO.getId() +"Does not exist");
+        }
         repo.save(transformer.toCustomerEntity(customerDTO));
     }
 
     @Override
     public void deleteCustomer(String id) {
+        if (!repo.existsById(id)){
+            throw new RuntimeException("Delete Filled Id :" + id +"Does not exist");
+        }
         repo.deleteById(id);
     }
 }

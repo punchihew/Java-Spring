bf44792597f28d092809289c7ec046c376fc8612
punchihewa <navishkapunchihewa66@gmail.com>
2024-04-04 08:54:42 +0530

    update

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/config/JPAConfig.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/config/JPAConfig.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManagerFactory;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -26,13 +27,19 @@ import javax.sql.DataSource;
 @EnableJpaRepositories(basePackages = "lk.ijse.gdse66.spring.repositories")
 public class JPAConfig {
 
+    Environment env;
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dmds = new DriverManagerDataSource();
-        dmds.setUrl("jdbc:mysql://localhost:3306/gdse66_spring_web?createDatabaseIfNotExist=true");
-        dmds.setUsername("root");
-        dmds.setPassword("1234");
-        dmds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dmds.setUrl(env.getRequiredProperty("spring.datasource.url"));
+        dmds.setUsername(env.getRequiredProperty("spring.datasource.username"));
+        dmds.setPassword(env.getRequiredProperty("spring.datasource.password"));
+        dmds.setDriverClassName(env.getRequiredProperty("spring.datasource.driverClassName"));
+//        dmds.setUrl("jdbc:mysql://localhost:3306/gdse66_spring_web?createDatabaseIfNotExist=true");
+//        dmds.setUsername("root");
+//        dmds.setPassword("1234");
+//        dmds.setDriverClassName("com.mysql.cj.jdbc.Driver");
         return dmds;
     }
 
@@ -43,7 +50,8 @@ public class JPAConfig {
         vendorAdapter.setGenerateDdl(true);
         vendorAdapter.setShowSql(true);
         vendorAdapter.setDatabase(Database.MYSQL);
-        vendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        vendorAdapter.setDatabasePlatform(env.getRequiredProperty("spring.jpa.hibernate.dialect"));
+//        vendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
 
         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
         factory.setJpaVendorAdapter(vendorAdapter);

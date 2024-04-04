af3c11a762e0b14154d032d3cba1aaa1c586e54d
punchihewa <navishkapunchihewa66@gmail.com>
2024-04-04 09:45:49 +0530

    update

---
+++ Spring-Web-Project/src/test/java/lk/ijse/gdse66/spring/service/impl/CustomerServiceImplTest.java
@@ -0,0 +1,45 @@
+package lk.ijse.gdse66.spring.service.impl;
+
+import lk.ijse.gdse66.spring.config.WebRootConfig;
+import lk.ijse.gdse66.spring.dto.CustomerDto;
+import lk.ijse.gdse66.spring.service.CustomerService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith({SpringExtension.class})
+@ContextConfiguration(classes = WebRootConfig.class)
+//@SpringJUnitConfig
+class CustomerServiceImplTest {
+  
+    @Autowired
+    CustomerService customerService;
+
+    @Test  //test case ekak
+    void getAllCustomers() {
+    }
+
+    @Test  //test case ekak
+    void getCustomerDetails() {
+    }
+
+    @Test
+    void saveCustomer() {
+        new CustomerDto("C001","navishka","Galle","Profilel");
+        CustomerDto customer = customerService.saveCustomer(CustomerDto);
+        
+    }
+
+    @Test
+    void updateCustomer() {
+    }
+
+    @Test
+    void deleteCustomer() {
+    }
+}

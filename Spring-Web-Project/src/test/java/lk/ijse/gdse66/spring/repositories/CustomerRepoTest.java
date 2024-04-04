1df800faa3d0271cb791a57f82b21ec27f8cf12a
punchihewa <navishkapunchihewa66@gmail.com>
2024-04-04 11:45:01 +0530

    updeta

--- Spring-Web-Project/src/test/java/lk/ijse/gdse66/spring/repositories/CustomerRepoTest.java
+++ Spring-Web-Project/src/test/java/lk/ijse/gdse66/spring/repositories/CustomerRepoTest.java
@@ -1,7 +1,28 @@
 package lk.ijse.gdse66.spring.repositories;
 
+import lk.ijse.gdse66.spring.config.WebRootConfig;
+import lk.ijse.gdse66.spring.entity.Customer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith({SpringExtension.class})
+@ContextConfiguration(classes = WebRootConfig.class)
 class CustomerRepoTest {
+
+    @Autowired
+    CustomerRepo customerRepo;
+
+    @Test
+    void saveCustomer(){
+        Customer customer = new Customer("C001", "Navishka", "Galle", "Photo");
+        Customer saveCustomer = customerRepo.save(customer);
+
+        assertNotNull(saveCustomer);
+    }
 
 }

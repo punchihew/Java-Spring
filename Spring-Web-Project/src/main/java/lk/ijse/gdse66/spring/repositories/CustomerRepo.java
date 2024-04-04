8b6c0106126477e986b44f4d2a7870369bc0752e
punchihewa <navishkapunchihewa66@gmail.com>
2024-04-04 12:18:25 +0530

    updeta

--- Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/repositories/CustomerRepo.java
+++ Spring-Web-Project/src/main/java/lk/ijse/gdse66/spring/repositories/CustomerRepo.java
@@ -3,11 +3,11 @@ package lk.ijse.gdse66.spring.repositories;
 import lk.ijse.gdse66.spring.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * @version: v0.0.1
- * @author: LakshanR
- * @date: 3/25/2024
- */
+import java.util.List;
+
 public interface CustomerRepo extends JpaRepository<Customer,String> {
+
+
+ List<Customer> findCustomersByName(String name);
 }
 
--- Spring-Web-Project/src/test/java/lk/ijse/gdse66/spring/repositories/CustomerRepoTest.java
+++ Spring-Web-Project/src/test/java/lk/ijse/gdse66/spring/repositories/CustomerRepoTest.java
@@ -17,6 +17,16 @@ class CustomerRepoTest {
     @Autowired
     CustomerRepo customerRepo;
 
+    void addAllCustomers(){
+        Customer customer1 = new Customer("C001", "Kasun Sampath", "Galle", "ProfilePic1");
+        Customer customer2 = new Customer("C002", "Saman Kumara", "Matara", "ProfilePic2");
+        Customer customer3 = new Customer("C003", "Amal Perera", "Panadura", "ProfilePic3");
+
+        customerRepo.save(customer1);
+        customerRepo.save(customer2);
+        customerRepo.save(customer3);
+    }
+
     @Test
     void saveCustomer(){
         Customer customer = new Customer("C001", "Navishka", "Galle", "Photo");
